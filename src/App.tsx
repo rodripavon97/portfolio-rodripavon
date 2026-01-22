31c5e14628d432f2f473efeea312da5df47a19b9
Rodrigo Pavon Gomez <rpavongomez@estudiantes.unsam.edu.ar>
2026-01-22 20:05:10 -0300

    add images and link

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { useActiveSection } from './hooks/useActiveSection';
 import { Github, ExternalLink } from 'lucide-react'; import { Header } from './components/header/header';
 import { HeroSection } from './components/heroSection/heroSection';
 import { Experience } from './components/experience/experience';
+import nochesMagicas from '../assets/Captura de pantalla de 2026-01-22 19-33-53.png';
+import gamerly from '../assets/IMG-20250210-WA0058.jpg';
+import accelerometer from '../assets/image.png';
+import accessibleApp from '../assets/accessible.jpg';
+import voleyapp from '../assets/image copy.png';
 
 const Portfolio = () => {
   const activeSection = useActiveSection();
@@ -10,44 +15,50 @@ const Portfolio = () => {
     {
       title: "Gamerly",
       description: "App social para gamers ocacionales que quieran evitar el bullying",
-      image: "https://images.unsplash.com/photo-1557821552-17105176677c?w=400&h=300&fit=crop",
-      link: "#",
+      image: gamerly,
+      link: "https://github.com/Siuss/gamerly-frontend",
       tech: "React Native, Zustand, CSS3, Expo,"
     },
     {
       title: "Noches Magicas",
       description: "Tp que abarca los 4 tipos de bases de datos + front",
-      image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?w=400&h=300&fit=crop",
-      link: "#",
+      image: nochesMagicas,
+      link: "https://github.com/rodripavon97/noches-magicas-2024-phm-front",
       tech: "React, Zustand, Kotlin, Springboot, Docker"
     },
     {
       title: "Gamerly",
       description: "App social para gamers ocacionales que quieran evitar el bullying",
-      image: "https://images.unsplash.com/photo-1557821552-17105176677c?w=400&h=300&fit=crop",
-      link: "#",
+      image: gamerly,
+      link: "https://github.com/Siuss/gamerly-backend",
       tech: "Kotlin, JPA, Springboot, Docker"
     },
     {
       title: "eg-paises-react ",
       description: "Migracion y explicacion de Debounce en react para busquedas e inputs",
       image: "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?w=400&h=300&fit=crop",
-      link: "#",
+      link: "https://github.com/rodripavon97/eg-paises-react",
       tech: "React, TypeScript"
     },
     {
       title: "acceledometro ",
       description: "Ejemplos de uso de acceledometro en Angular",
-      image: "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?w=400&h=300&fit=crop",
-      link: "#",
+      image: accelerometer,
+      link: "https://github.com/rodripavon97/acelerometro-ionic-angular",
       tech: "Angular, TypeScript, Ionic"
     },
     {
       title: "Accesible app",
       description: "Ejemplo de accesibilidad de gestos para personas con capacidades nula de vista - ",
-      image: "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?w=400&h=300&fit=crop",
-      link: "#",
+      image: accessibleApp,
+      link: "https://github.com/rodripavon97/accessible-app",
       tech: "React Native, Expo, TypeScript"
+    },{
+      title: "voleyapp",
+      description: "Webapp de contador de puntos de voley",
+      image: voleyapp,
+      link: "https://github.com/rodripavon97/voleyapp",
+      tech: "React, TypeScript, TailwindCSS"
     }
   ];
 
@@ -58,8 +69,8 @@ const Portfolio = () => {
       <div className="fixed inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 -z-10"></div>
       {/* Header */}
       <Header activeSection={activeSection} />
-
-      <div id="about" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-16">
+      <div id="about" className="w-full pt-24 pb-16">
+      <div className="max-w-[1920px] mx-auto px-4 sm:px-6 lg:px-8">
         {/* Hero Section */}
         <HeroSection />
 
@@ -103,13 +114,13 @@ const Portfolio = () => {
             </p>
           </div>
         </section>
-
+        </div>
+        </div>
         {/* Footer */}
         <footer className="text-center text-gray-500 text-sm pt-8 border-t border-gray-800">
           <p> {new Date().getFullYear()} © Rodrigo Pavon</p>
         </footer>
       </div>
-    </div>
   );
 };
 
